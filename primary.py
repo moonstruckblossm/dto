fc79ad847b922a4e9ac29c13aff1bd081fd1e55d
moonstruckblossm <sm5069@rit.edu>
2021-07-01 13:43:14 -0400

    complete

--- primary.py
+++ primary.py
@@ -11,7 +11,9 @@
 #       1 - create a print statement that prints the sum of two numbers
 #       2 - create a print statement that prints the sum of three numbers
 #       3 - create a print statement the prints the sum of two negative numbers
-
+print(3+5)
+print(9+6+27)
+print(-9+-3)
 
 
 # subtraction
@@ -19,7 +21,9 @@
 #       1 - create a print statement that prints the difference of two numbers
 #       2 - create a print statement that prints the difference of three numbers
 #       3 - create a print statement the prints the difference of two negative numbers
-
+print(9-3)
+print(9-7-8)
+print(-9-(-8))
 
 
 # multiplication and true division
@@ -27,25 +31,27 @@
 #       1 - create a print statement the prints the product of two numbers
 #       2 - create a print statement that prints the dividend of two numbers
 #       3 - create a print statement that evaluates an operation using both multiplication and division
-
+print(3*9)
+print(9/4)
+print((9/3)*6)
 
 
 # floor division
 #   instructions
 #       1 - using floor division, print the dividend of two numbers.
-
+print(8//3)
 
 
 # exponentiation
 #   instructions
 #       1 - using exponentiation, print the power of two numbers
-
+print(5**8)
 
 
 # modulus
 #   instructions
 #       1 - using modulus, print the remainder of two numbers
-
+print(27%5)
 
 # --------------- Section 2 --------------- #
 # ---------- String Concatenation --------- #
@@ -55,15 +61,18 @@
 #       1 - print the concatenation of your first and last name
 #       2 - print the concatenation of five animals you like
 #       3 - print the concatenation of each word in a phrase
-
-
+print("kai"+"mitchell")
+print("snail"+"snail"+"snail"+"snail"+"snail")
+print("it"+"aint"+"over"+"yet")
 
 # duplication
 #   instructions
 #       1 - print the duplpication of your first 5 times
 #       2 - print the duplication of a song you like 10 times
-
+print("kai"*4)
+print("buttercup"*10)
 
 # concatenation and duplpication
 #   instructions
 #       1 - print the concatenation of two strings duplicated 3 times each
+print(("die"+"house")*3)
--- secondary.py
+++ secondary.py
@@ -13,8 +13,12 @@
 # $$$
 # $$$$
 # $$$$$
-
-
+print("$")
+print("$"*2)
+print("$"*3)
+print("$"*4)
+print("$"*5)
+print()
 
 # create the following pattern using string duplication and print statments:
 #
@@ -23,8 +27,13 @@
 # $$$
 # $$
 # $
+print("$"*5)
+print("$"*4)
+print("$"*3)
+print("$"*2)
+print("$")
 
-
+print()
 
 # create the following pattern using string duplication and print statments:
 #
@@ -34,8 +43,27 @@
 #  $$$$
 # $$$$$
 
+#though they are not visible, the spaces before each dollar sign are characters
+#for each line you print, there is a certain number of spaces and a certain number of dollar signs
+#either way, you'll want to have FIVE characters on each line that you print, so how can we approach this?
+
+#think about how many dollar signs are on each line. if you need five characters on each line, but there's less than five
+#dollar signs on a line, what will you do?
+
+#remember that both spaces (" ") and dollar signs ($) are characters!
+
+#FIVE characters in total - the number of dollar signs you want = the amount of spaces to put before the dollar signs!
+
+#for example, if you want THREE dollar signs, you'll want to put TWO spaces before them, since three plus two is five.
+#your total number of characters five.
 
 
+print((" "*4)+"$"*1)
+print((" "*3)+"$"*2)
+print((" "*2)+"$"*3)
+print((" "*1)+"$"*4)
+print((" "*0)+"$"*5)
+print()
 # create the following pattern using string duplication and print statments:
 #
 # $$$$$
@@ -43,3 +71,12 @@
 #   $$$
 #    $$
 #     $
+
+#this is very similar to the previous problem! you'll just want to do the same thing in the opposite order to get a
+#right-oriented upside down right triangle!
+
+print((" "*0)+"$"*5)
+print((" "*1)+"$"*4)
+print((" "*2)+"$"*3)
+print((" "*3)+"$"*2)
+print((" "*4)+"$"*1)
